17109beeafae40abf802825136d5727ee5fe1e7c
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-19 23:17:14 +0530

    added theam

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+interface props{
+    children:React.ReactNode
+}
+
+export default function AuthLayout({children}:props) {
+  return (
+    <div className="relative flex min-h-svh flex-col items-center justify-center">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+            {children}
+        </div>
+    </div>
+  )
+}
--- app/(auth)/signIn/page.tsx
+++ app/(auth)/signIn/page.tsx
@@ -1,17 +1,26 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
 
 export default function page() {
   return (
     <div>
       <Card>
         <CardHeader>
-          <CardTitle>
+          <CardTitle className="text-xl ">
             signIn
           </CardTitle>
           <CardDescription>
             login in with github
           </CardDescription>
         </CardHeader>
+
+        <CardContent>
+          <Button className="w-full" variant={"outline"}>
+            <GithubIcon className="size-4"/>
+            sign in with GitHb
+          </Button>
+        </CardContent>
       </Card>
     </div>
   )

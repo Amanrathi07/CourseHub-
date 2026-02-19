7c4b686a9fb09193c0809e18e0f5a80f5b84f3b3
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-19 23:37:44 +0530

    tcss update

--- app/(auth)/signIn/page.tsx
+++ app/(auth)/signIn/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function page() {
@@ -20,6 +22,17 @@ export default function page() {
             <GithubIcon className="size-4"/>
             sign in with GitHb
           </Button>
+
+          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-card px-2 text-muted-foreground">or continue with</span>
+          </div>
+
+          <div className="grid gap-3">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" placeholder="abc@example.com" />
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>

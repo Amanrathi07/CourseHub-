c7799459d0a9865c21008e9f6ea495c2a74b929c
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-21 00:02:38 +0530

    t3 env setupp

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,15 +2,27 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "@/lib/env";
 
-const adapter = new PrismaPg({connectionString:process.env.DATABASE_URL!});
+const adapter = new PrismaPg({connectionString:env.DATABASE_URL});
 
 const prisma = new PrismaClient({adapter});
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+
      emailAndPassword: { 
     enabled: true, 
   }, 
+    socialProviders: {
+        google: { 
+            clientId: env.GOOGLE_CLIENT_ID, 
+            clientSecret: env.GOOGLE_CLIENT_SECRET, 
+        }, 
+         github: { 
+            clientId: env.GITHUB_CLIENT_ID, 
+            clientSecret: env.GITHUB_CLIENT_SECRET, 
+        }, 
+    },
 });

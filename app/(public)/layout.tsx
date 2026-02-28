1aa1645753f9c85313b3a45116c00b74f1e8e171
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-28 23:25:24 +0530

    1 m t

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,7 @@ interface Props{
     children:React.ReactNode ;
 }
 
-export default function LayoutPublic({children}:Props) {
+export default function layout({children}:Props) {
   return (
     <div>{children}</div>
   )

0b0b9237137e0c42f3655333e04b7d4b9687a6ef
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-01 22:57:10 +0530

    desktop navigation

--- app/(public)/_components/NavBar.tsx
+++ app/(public)/_components/NavBar.tsx
@@ -1,6 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <header  className="sticky  top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
+        <div className="container flex min-h-16 items-center mx-auto px4 md:px-6 lg:px-8">
+            <Link className="flex items-center space-x-2 mr-4" href="">
+                <Image src={""} alt="Logo" className="size-9"/>
+                <span className="font-bold">Course Hub</span>
+            </Link>
+
+            {/*  desktop navigation */}
+            <nav>
+
+            </nav>
+        </div>
+    </header>
   )
 }

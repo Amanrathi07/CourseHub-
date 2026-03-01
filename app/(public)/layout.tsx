7a3940b51cb2488846889bfa9dd43b5831367027
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-01 22:46:43 +0530

    navbar.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from "./_components/NavBar";
 
 interface Props{
     children:React.ReactNode ;
@@ -5,6 +6,12 @@ interface Props{
 
 export default function layout({children}:Props) {
   return (
-    <div>{children}</div>
+    <div>
+      <NavBar />
+      <main className="container mx-auto px-4 md:px-6 lg:px-8">
+         {children}
+      </main>
+     
+    </div>
   )
 }

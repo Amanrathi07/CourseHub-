49e6e4b6fe885ec7e70436f9ec3f2dc12f099377
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-08 22:39:26 +0530

    down dropdown

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -2,6 +2,7 @@
 
 import {
   IconCreditCard,
+  IconDashboard,
   IconDotsVertical,
   IconLogout,
   IconNotification,
@@ -29,6 +30,8 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 import { authClient } from "@/lib/auth-client"
+import Link from "next/link"
+import { HomeIcon, Tv2 } from "lucide-react"
 
 export function NavUser() {
 
@@ -100,17 +103,23 @@ export function NavUser() {
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <IconUserCircle />
-                Account
+              <DropdownMenuItem asChild>
+               <Link href="/">
+                  <HomeIcon />
+                  Homepage
+               </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconCreditCard />
-                Billing
+             <DropdownMenuItem asChild>
+               <Link href="/admin">
+                  <IconDashboard />
+                  Dashboard
+               </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconNotification />
-                Notifications
+              <DropdownMenuItem asChild>
+               <Link href="/dashboard">
+                  <Tv2 />
+                  Courses
+               </Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />

bf6318921505c367313cc467cf8bebade378e7df
Amanrathi07 <amanrathi4321@gmail.com>
2026-03-10 22:49:15 +0530

    validation client

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -1,9 +1,33 @@
-import { Card } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function page() {
   return (
-    <Card>
-        
-    </Card>
+    <>
+        <div className="flex items-center gap-4">
+            <Link href={"/admin/courses"} className={buttonVariants({
+                variant:"outline" ,
+                size:"icon"
+            })} >
+                <ArrowLeft className="size-4"/>
+            </Link>
+            <h1 className="text-2xl font-bold">
+                Create Course
+            </h1>
+        </div>
+
+        <Card>
+            <CardHeader>
+                <CardTitle>Basic Information</CardTitle>
+                <CardDescription> Provide basic Information about the course</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                
+            </CardContent>
+        </Card>
+    </>
   )
 }
